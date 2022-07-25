a5c3cf8fce439a456e367484249ea336449681c3
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-25 16:59:11 +0900

    초기 커밋.

---
+++ day34_01/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace day34_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.BTN_CHECKORDER1 = new System.Windows.Forms.Button();
+            this.BTN_CHECKORDER2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(360, 188);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(428, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.Size = new System.Drawing.Size(360, 188);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // BTN_CHECKORDER1
+            // 
+            this.BTN_CHECKORDER1.Location = new System.Drawing.Point(13, 207);
+            this.BTN_CHECKORDER1.Name = "BTN_CHECKORDER1";
+            this.BTN_CHECKORDER1.Size = new System.Drawing.Size(136, 29);
+            this.BTN_CHECKORDER1.TabIndex = 2;
+            this.BTN_CHECKORDER1.Text = "아이디로 찾기";
+            this.BTN_CHECKORDER1.UseVisualStyleBackColor = true;
+            this.BTN_CHECKORDER1.Click += new System.EventHandler(this.BTN_CHECKORDER1_Click);
+            // 
+            // BTN_CHECKORDER2
+            // 
+            this.BTN_CHECKORDER2.Location = new System.Drawing.Point(428, 206);
+            this.BTN_CHECKORDER2.Name = "BTN_CHECKORDER2";
+            this.BTN_CHECKORDER2.Size = new System.Drawing.Size(145, 29);
+            this.BTN_CHECKORDER2.TabIndex = 3;
+            this.BTN_CHECKORDER2.Text = "주문번호로 찾기";
+            this.BTN_CHECKORDER2.UseVisualStyleBackColor = true;
+            this.BTN_CHECKORDER2.Click += new System.EventHandler(this.BTN_CHECKORDER2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTN_CHECKORDER2);
+            this.Controls.Add(this.BTN_CHECKORDER1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button BTN_CHECKORDER1;
+        private System.Windows.Forms.Button BTN_CHECKORDER2;
+    }
+}
+
---
+++ day34_01/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day34_01
+{
+    public partial class Form1 : Form
+    {
+        DataSet dataSet1;
+        DataRelation dataRelation1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataSet1 = new DataSet("info");
+            dataSet1.Tables.Add(new DataTable("Table1"));
+            dataSet1.Tables.Add(new DataTable("Table2"));
+
+            //table1
+
+            //Column생성
+            DataColumn Col1;
+            Col1 = new DataColumn("id", typeof(int));
+            Col1.Unique = true;
+            dataSet1.Tables[0].Columns.Add(Col1); //  dataSet1.Tables["Table1"].~~~~ 랑 같다
+            dataSet1.Tables[0].PrimaryKey = new DataColumn[] { Col1 };
+
+            Col1 = new DataColumn("pwd", typeof(string));
+            Col1.MaxLength = 16;
+            dataSet1.Tables[0].Columns.Add(Col1);
+
+            Col1 = new DataColumn("name", typeof(string));
+            Col1.MaxLength = 5;
+            dataSet1.Tables[0].Columns.Add(Col1);
+
+            //Row생성
+            DataRow row1;
+            row1 = dataSet1.Tables[0].NewRow();
+            row1[0] = 201984001;
+            row1[1] = "password";
+            row1[2] = "강동훈";
+            dataSet1.Tables[0].Rows.Add(row1);
+
+            row1 = dataSet1.Tables[0].NewRow();
+            row1[0] = 22220329;
+            row1[1] = "alenee";
+            row1[2] = "이준상";
+            dataSet1.Tables[0].Rows.Add(row1);
+
+            row1 = dataSet1.Tables[0].NewRow();
+            row1[0] = 20220011;
+            row1[1] = "irisas";
+            row1[2] = "김훈";
+            dataSet1.Tables[0].Rows.Add(row1);
+
+
+            //table2
+
+            //Column생성
+            DataColumn Col2;
+            Col2 = new DataColumn("주문번호", typeof(int));
+            Col2.Unique = true;
+            dataSet1.Tables[1].Columns.Add(Col2); 
+            dataSet1.Tables[1].PrimaryKey = new DataColumn[] { Col2 };
+
+            Col2 = new DataColumn("상품명", typeof(string));
+            Col2.MaxLength = 16;
+            dataSet1.Tables[1].Columns.Add(Col2);
+
+            Col2 = new DataColumn("상품수량", typeof(int));
+            dataSet1.Tables[1].Columns.Add(Col2);
+
+            Col2 = new DataColumn("고객번호", typeof(int));
+            dataSet1.Tables[1].Columns.Add(Col2);
+            ForeignKeyConstraint FK = new ForeignKeyConstraint("FK", dataSet1.Tables[0].Columns["id"], Col2);
+
+            //Row생성
+            row1 = dataSet1.Tables[1].NewRow();
+            row1[0] = 2022072501;
+            row1[1] = "사과";
+            row1[2] = 6;
+            row1[3] = 201984001;
+            dataSet1.Tables[1].Rows.Add(row1);
+
+            row1 = dataSet1.Tables[1].NewRow();
+            row1[0] = 2022072502;
+            row1[1] = "바나나";
+            row1[2] = 2;
+            row1[3] = 201984001;
+            dataSet1.Tables[1].Rows.Add(row1);
+
+            row1 = dataSet1.Tables[1].NewRow();
+            row1[0] = 2022072503;
+            row1[1] = "사과";
+            row1[2] = 1;
+            row1[3] = 22220329;
+            dataSet1.Tables[1].Rows.Add(row1);
+
+            row1 = dataSet1.Tables[1].NewRow();
+            row1[0] = 2022072504;
+            row1[1] = "설탕";
+            row1[2] = 2;
+            row1[3] = 20220011;
+            dataSet1.Tables[1].Rows.Add(row1);
+
+            row1 = dataSet1.Tables[1].NewRow();
+            row1[0] = 2022072505;
+            row1[1] = "사과";
+            row1[2] = 8;
+            row1["고객번호"] = 20220011;
+            dataSet1.Tables[1].Rows.Add(row1);
+
+            //객체를 통한 관계 설정
+            dataRelation1 = new DataRelation("ORDER", dataSet1.Tables[0].Columns["id"], dataSet1.Tables[1].Columns["고객번호"]);
+            dataSet1.Relations.Add(dataRelation1);
+            dataGridView1.DataSource = dataSet1.Tables[0];
+            dataGridView2.DataSource = dataSet1.Tables[1];
+        }
+
+        private void BTN_CHECKORDER1_Click(object sender, EventArgs e)
+        {
+            int chkId = (int)dataGridView1.CurrentRow.Cells[0].Value;
+            DataRow[] people = dataSet1.Tables[0].Select("id ="+ chkId);
+            DataRow[] Items = people[0].GetChildRows("ORDER");
+            string Fruit = "";
+            if(Items.Length > 0)
+            {
+                foreach(DataRow row in Items)
+                {
+                    Fruit += "주문번호: " + row["주문번호"].ToString();
+                    Fruit += "  상품명: " + row["상품명"].ToString();
+                    Fruit += "  상품수량: " + row["상품수량"].ToString();
+                    Fruit += "\n";
+                }
+                MessageBox.Show(Fruit,"상품주문");
+            }
+        }
+
+        private void BTN_CHECKORDER2_Click(object sender, EventArgs e)
+        {
+            int chkId = (int)dataGridView2.CurrentRow.Cells["고객번호"].Value;
+            DataRow[] people = dataSet1.Tables[1].Select("고객번호 =" + chkId);
+            DataRow Items = people[0].GetParentRow("ORDER");
+            string odr = "이름: " + Items["name"].ToString();
+                MessageBox.Show(odr, "상품주문");           
+        }
+    }
+}
